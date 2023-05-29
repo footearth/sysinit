503509b6f7953eac87ae51b6a45d29ffd6a5a202
footearth <tencentcloud@cotfea.co>
2023-05-29 13:09:57 +0800

    update task version

--- Task.sh
+++ Task.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-# wget -O task.tar.gz https://github.com/go-task/task/releases/download/v3.24.0/task_linux_amd64.tar.gz
-# wget -O task.tar.gz https://kgithub.com/go-task/task/releases/download/v3.24.0/task_linux_amd64.tar.gz
-wget -O task.tar.gz https://hub.fgit.ml/go-task/task/releases/download/v3.24.0/task_linux_amd64.tar.gz
+# wget -O task.tar.gz https://github.com/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
+# wget -O task.tar.gz https://kgithub.com/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
+wget -O task.tar.gz https://hub.fgit.ml/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
 mkdir ./task
 tar -C ./task -xvf task.tar.gz
