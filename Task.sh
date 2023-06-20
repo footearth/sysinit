9e9664550e788509b243be017673c07d485690a3
footearth <footearth@gmail.com>
2023-06-20 10:52:19 +0800

    update git proxy

--- Task.sh
+++ Task.sh
@@ -2,8 +2,9 @@
 
 # https://github.com/go-task/task/releases
 
-# wget -O task.tar.gz https://github.com/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
-# wget -O task.tar.gz https://hub.fgit.ml/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
-wget -O task.tar.gz https://kgithub.com/go-task/task/releases/download/v3.25.0/task_linux_amd64.tar.gz
+# wget -O task.tar.gz https://github.com/go-task/task/releases/download/v3.26.0/task_linux_amd64.tar.gz
+# wget -O task.tar.gz https://hub.fgit.ml/go-task/task/releases/download/v3.26.0/task_linux_amd64.tar.gz
+# wget -O task.tar.gz https://kgithub.com/go-task/task/releases/download/v3.26.0/task_linux_amd64.tar.gz
+wget -O task.tar.gz http://ghproxy.com/https://github.com/go-task/task/releases/download/v3.26.0/task_linux_amd64.tar.gz
 mkdir ./task
 tar -C ./task -xvf task.tar.gz
