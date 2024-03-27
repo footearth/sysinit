658c83b1f9b02f48c07c0335244084c23262bc47
footearth <footearth@gmail.com>
2024-03-27 13:36:31 +0800

    update & adjust

--- Task.sh
+++ Task.sh
@@ -5,8 +5,8 @@
 # https://github.com/go-task/task/releases
 # https://hub.fgit.ml
 # https://kgithub.com
-export TASK_VERSION=3.29.1 \
+export TASK_VERSION=3.35.1 \
 &&  wget -O task.tar.gz \
-    http://ghproxy.com/https://github.com/go-task/task/releases/download/v${TASK_VERSION}/task_linux_amd64.tar.gz
+    http://mirror.ghproxy.com/https://github.com/go-task/task/releases/download/v${TASK_VERSION}/task_linux_amd64.tar.gz
 mkdir ./task
 tar -C ./task -xvf task.tar.gz
