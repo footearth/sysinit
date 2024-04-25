48a3f9a34ea7aaf1f83ddd07ac6b0ba005cdd175
footearth <footearth@gmail.com>
2024-04-25 11:17:18 +0800

    update task version & update arch task

--- Task.sh
+++ Task.sh
@@ -5,7 +5,7 @@
 # https://github.com/go-task/task/releases
 # https://hub.fgit.ml
 # https://kgithub.com
-export TASK_VERSION=3.35.1 \
+export TASK_VERSION=3.36.0 \
 &&  wget -O task.tar.gz \
     http://mirror.ghproxy.com/https://github.com/go-task/task/releases/download/v${TASK_VERSION}/task_linux_amd64.tar.gz
 mkdir ./task
